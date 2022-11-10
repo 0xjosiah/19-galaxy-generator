7acebd4796ef8d99d91cf54b57b0ce353b70bf65
Josiah Webb <jj.webb520@gmail.com>
2022-11-10 16:06:06 -0700

    adds setInstance method of camera

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -1,7 +1,20 @@
 import * as THREE from 'three'
+import Experience from './Experience'
 
 export default class Camera {
     constructor() {
-        this.camera = new THREE.Camera()
+        this.experience = new Experience()
+        this.sizes = this.experience.sizes
+        this.scene = this.experience.scene
+        this.canvas = this.experience.canvas
+
+        this.setInstance()
+        this.setOrbitControls()
+    }
+
+    setInstance() {
+        this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, .1, 100)
+        this.instance.position.set(3, 3, 3)
+        this.scene.add(this.instance)
     }
 }
