c7ba80fb47fe3aaa4df1c372c8ba7223ab56dd18
Josiah Webb <jj.webb520@gmail.com>
2022-11-10 15:45:03 -0700

    creates time class, adds time class to experience

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,9 +1,11 @@
 import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
 
 export default class Experience {
     constructor(canvas) {
 
         // Setup
         this.sizes = new Sizes()
+        this.time = new Time()
     }
 }
