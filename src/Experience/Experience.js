0eef4726ded2b54f04207f60570f6f342f39f7c3
Josiah Webb <jj.webb520@gmail.com>
2022-11-10 15:56:00 -0700

    makes experience class singleton, adds resize and update methods to experience class

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,11 +1,34 @@
 import Sizes from "./Utils/Sizes";
 import Time from "./Utils/Time";
 
+let instance = null
+
 export default class Experience {
     constructor(canvas) {
+
+        // Singleton
+        if(instance) return instance
+        instance = this
+
+        // Options
+        this.canvas = canvas
 
         // Setup
         this.sizes = new Sizes()
         this.time = new Time()
+
+        // Resize event
+        this.sizes.on('resize', () => this.resize())
+
+        // Tick event
+        this.time.on('tick', () => this.update())
+    }
+
+    resize() {
+        // console.log('resize event');
+    }
+
+    update() {
+        // console.log('update event');
     }
 }
