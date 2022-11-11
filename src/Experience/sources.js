0dff975ac9c8611637b973559232d358ea34d62a
Josiah Webb <jj.webb520@gmail.com>
2022-11-10 17:16:42 -0700

    creates sources.js

---
+++ src/Experience/sources.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: 'stars',
+        type: 'texture',
+        path: 'textures/particles/4.png'
+    },
+    {
+        name: 'spaceDust',
+        type: 'texture',
+        path: 'textures/particles/12.png'
+    }
+]
