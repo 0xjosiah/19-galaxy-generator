108719e0e768bcd5fc6fcb2d004adb264992d417
Josiah Webb <jj.webb520@gmail.com>
2022-11-10 17:56:40 -0700

    fills out backdrop class

--- src/Experience/Scene/Backdrop.js
+++ src/Experience/Scene/Backdrop.js
@@ -1,5 +1,51 @@
+import * as THREE from 'three'
+import Experience from "../Experience";
+
 export default class Backdrop {
-    constructor() {
-        console.log('backdrop');
+    constructor(count) {
+        this.count = count
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        this.geometry = new THREE.BufferGeometry()
+        this.positions = new Float32Array(this.count * 3)
+        this.colors = new Float32Array(this.count * 3)
+
+        this.setPositions()
+        this.setColors()
+        this.setMaterial()
+        this.setPoints()
+    }
+
+    setPositions() {
+        this.positions.forEach((item, index) => {
+            return this.positions[index] = (Math.random() - .5) * 75
+        })
+
+        this.geometry.setAttribute('position', new THREE.BufferAttribute(this.positions, 3))
+    }
+
+    setColors() {
+        this.colors.forEach((item, index) => {
+            return this.colors[index] = Math.random()
+        })
+
+        this.geometry.setAttribute('color', new THREE.BufferAttribute(this.colors, 3))
+    }
+
+    setMaterial() {
+        this.material = new THREE.PointsMaterial({
+            size: .04,
+            sizeAttenuation: true,
+            depthWrite: false,
+            alphaMap: this.resources.items.spaceDust,
+            blending: THREE.AdditiveBlending,
+            vertexColors: true,
+        })
+    }
+
+    setPoints() {
+        this.points = new THREE.Points(this.geometry, this.material)
+        this.scene.add(this.points)
     }
 }
--- src/Experience/Scene/Universe.js
+++ src/Experience/Scene/Universe.js
@@ -8,8 +8,10 @@ export default class Universe {
         this.scene = this.experience.scene
         this.resources = this.experience.resources
 
+        this.backdropStarsCount = 30000
+
         this.resources.on('ready', () => {
-            this.backdrop = new Backdrop()
+            this.backdrop = new Backdrop(this.backdropStarsCount)
         })
     }
 }
