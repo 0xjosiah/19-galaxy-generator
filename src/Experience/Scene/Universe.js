121adbe20fd2ae517c742a458735b89b1704cd6e
Josiah Webb <jj.webb520@gmail.com>
2022-11-15 17:52:15 -0700

    gui now changes galaxy params, except blackhole

--- src/Experience/Scene/Universe.js
+++ src/Experience/Scene/Universe.js
@@ -32,18 +32,42 @@ export default class Universe {
             this.galaxy = new Galaxy(this.parameters)
         })
 
-        this.debugFolder.add(this.parameters, 'count', 100, 1000000, 100).onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.add(this.parameters, 'size', .001, .1, .001).onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.add(this.parameters, 'radius', .01, 20, .01).onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.add(this.parameters, 'branches', 2, 20, 1).onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.add(this.parameters, 'curvature', -5, 5, .01).onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.add(this.parameters, 'randomness', 0, 2, .01).onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.add(this.parameters, 'concentration', 1, 10, .1).onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.addColor(this.parameters, 'innerColor').onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.addColor(this.parameters, 'outerColor').onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.add(this.parameters, 'blackHole').onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.add(this.parameters, 'branchWaves').onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
-        this.debugFolder.add(this.parameters, 'starShape', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]).onFinishChange(() => this.galaxy.updateGalaxy(this.parameters))
+        this.debugFolder.add(this.parameters, 'count', 100, 1000000, 100).onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.add(this.parameters, 'size', .001, .1, .001).onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.add(this.parameters, 'radius', .01, 20, .01).onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.add(this.parameters, 'branches', 2, 20, 1).onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.add(this.parameters, 'curvature', -5, 5, .01).onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.add(this.parameters, 'randomness', 0, 2, .01).onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.add(this.parameters, 'concentration', 1, 10, .1).onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.addColor(this.parameters, 'innerColor').onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.addColor(this.parameters, 'outerColor').onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.add(this.parameters, 'blackHole').onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.add(this.parameters, 'branchWaves').onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
+        this.debugFolder.add(this.parameters, 'starShape', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]).onFinishChange(() => {
+            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+        })
     }
 
     // generateGalaxy() {
