2faf9bed40eb2d9f3ad8cc25623960247f478992
Josiah Webb <jj.webb520@gmail.com>
2022-11-16 16:34:04 -0700

    refactors galaxy class

--- src/Experience/Scene/Galaxy.js
+++ src/Experience/Scene/Galaxy.js
@@ -4,66 +4,67 @@ import BlackHole from './BlackHole'
 
 export default class Galaxy {
     
-    constructor(parameters) {
-        // const parameters = {
-        //     count: 100000,
-        //     size: .01,
-        //     radius: 5,
-        //     branches: 3,
-        //     curvature: 1,
-        //     randomness: .2,
-        //     concentration: 3,
-        //     innerColor: 0xff6030,
-        //     outerColor: 0x1b3984,
-        //     blackHole: true,
-        //     branchWaves: true,
-        //     starShape: 4,
-        //     backdropStarsCount: 10000
-        // }
-
-        this.count = parameters.count
-        this.size = parameters.size
-        this.radius = parameters.radius
-        this.branches = parameters.branches
-        this.curvature = parameters.curvature
-        this.randomness = parameters.randomness
-        this.concentration = parameters.concentration
-        this.innerColor = parameters.innerColor
-        this.outerColor = parameters.outerColor
-        this.branchWaves = parameters.branchWaves
-        this.isBlackHole = parameters.isBlackHole
-        this.starShape = parameters.starShape
-
+    constructor() {
+        
         this.experience = new Experience()
         this.resources = this.experience.resources
         this.scene = this.experience.scene
+        this.debug = this.experience.debug
+        this.debugFolder = this.debug.ui.addFolder('Galaxy')
 
-        this.geometry = new THREE.BufferGeometry()
-        this.positions = new Float32Array(this.count * 3)
-        this.colors = new Float32Array(this.count * 3)
+        this.parameters = {}
+        this.parameters.count = 100000
+        this.parameters.size = .01
+        this.parameters.radius = 5
+        this.parameters.branches = 3
+        this.parameters.curvature = 1
+        this.parameters.randomness = .2
+        this.parameters.concentration = 3
+        this.parameters.innerColor = 0xff6030
+        this.parameters.outerColor = 0x1b3984
+        this.parameters.branchWaves = false
+        this.parameters.isBlackHole = true
+        this.parameters.starShape = 4
 
-        this.colorInside = new THREE.Color(this.innerColor)
-        this.colorOutside = new THREE.Color(this.outerColor)
+        this.debugFolder.add(this.parameters, 'count', 100, 1000000, 100).onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.add(this.parameters, 'size', .001, .1, .001).onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.add(this.parameters, 'radius', .01, 20, .01).onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.add(this.parameters, 'branches', 2, 20, 1).onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.add(this.parameters, 'curvature', -5, 5, .01).onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.add(this.parameters, 'randomness', 0, 2, .01).onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.add(this.parameters, 'concentration', 1, 10, .1).onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.addColor(this.parameters, 'innerColor').onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.addColor(this.parameters, 'outerColor').onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.add(this.parameters, 'isBlackHole').onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.add(this.parameters, 'branchWaves').onFinishChange(() => this.updateGalaxy())
+        this.debugFolder.add(this.parameters, 'starShape', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]).onFinishChange(() => this.updateGalaxy())
 
-        this.setRandomAttributes()
+        this.setGeometry()
         this.setBlackHole()
         this.setMaterial()
         this.setPoints()
     }
 
-    setRandomAttributes() {
-        for(let i = 0; i < this.count; i++) {
+    setGeometry() {
+        this.geometry = new THREE.BufferGeometry()
+        this.positions = new Float32Array(this.parameters.count * 3)
+        this.colors = new Float32Array(this.parameters.count * 3)
+
+        this.colorInside = new THREE.Color(this.parameters.innerColor)
+        this.colorOutside = new THREE.Color(this.parameters.outerColor)
+
+        for(let i = 0; i < this.parameters.count; i++) {
             const i3 = i * 3
-            const radiusMultiplier = this.radius + Math.pow(Math.random(), this.concentration)
-            const randomRadius = Math.pow(Math.random(), this.concentration) * radiusMultiplier // this concentrates stars toward center of branches, adding addt'l math.pow helps prevent cross formation in center
+            const radiusMultiplier = this.parameters.radius + Math.pow(Math.random(), this.parameters.concentration)
+            const randomRadius = Math.pow(Math.random(), this.parameters.concentration) * radiusMultiplier // this concentrates stars toward center of branches, adding addt'l math.pow helps prevent cross formation in center
             
             // Positions
-            const curvatureAngle = randomRadius * this.curvature
-            const branchAngle = (i % this.branches) / this.branches * Math.PI * 2
+            const curvatureAngle = randomRadius * this.parameters.curvature
+            const branchAngle = (i % this.parameters.branches) / this.parameters.branches * Math.PI * 2
 
-            const randomX = Math.pow(Math.random(), this.concentration) * (Math.random() < .5 ? 1 : -1) * this.randomness * randomRadius
-            const randomY = Math.pow(Math.random(), this.concentration) * (Math.random() < .5 ? 1 : -1) * this.randomness * randomRadius
-            const randomZ = Math.pow(Math.random(), this.concentration) * (Math.random() < .5 ? 1 : -1) * this.randomness * randomRadius
+            const randomX = Math.pow(Math.random(), this.parameters.concentration) * (Math.random() < .5 ? 1 : -1) * this.parameters.randomness * randomRadius
+            const randomY = Math.pow(Math.random(), this.parameters.concentration) * (Math.random() < .5 ? 1 : -1) * this.parameters.randomness * randomRadius
+            const randomZ = Math.pow(Math.random(), this.parameters.concentration) * (Math.random() < .5 ? 1 : -1) * this.parameters.randomness * randomRadius
 
             // mess with these to make really cool effects
             const alternating = branchAngle < Math.PI ? 1 : -1
@@ -72,11 +73,11 @@ export default class Galaxy {
             const zPos = Math.sin(branchAngle + curvatureAngle) * randomRadius
 
             this.positions[i3 + 0] = xPos + randomX
-            this.positions[i3 + 1] = this.branchWaves ? yPos + randomY : randomY
+            this.positions[i3 + 1] = this.parameters.branchWaves ? yPos + randomY : randomY
             this.positions[i3 + 2] = zPos + randomZ
 
             // Colors
-            const mixedColor = this.colorInside.clone().lerp(this.colorOutside, randomRadius / this.radius)
+            const mixedColor = this.colorInside.clone().lerp(this.colorOutside, randomRadius / this.parameters.radius)
             this.colors[i3 + 0] = mixedColor.r
             this.colors[i3 + 1] = mixedColor.g
             this.colors[i3 + 2] = mixedColor.b
@@ -86,10 +87,10 @@ export default class Galaxy {
     }
 
     setMaterial() {
-        const stars = this.resources.items[`stars${this.starShape}`]
+        const stars = this.resources.items[`stars${this.parameters.starShape}`]
 
         this.material = new THREE.PointsMaterial({
-            size: this.size,
+            size: this.parameters.size,
             sizeAttenuation: true,
             depthWrite: false,
             alphaMap: stars,
@@ -104,23 +105,21 @@ export default class Galaxy {
     }
 
     setBlackHole() {
-        if(this.isBlackHole) {
+        if(this.parameters.isBlackHole) {
             this.blackHole = new BlackHole()
         }
-        // this.scene.add(this.blackHole)
     }
 
-    updateGalaxy(parameters) {
+    updateGalaxy() {
         this.geometry.dispose()
         this.material.dispose()
         this.scene.remove(this.points)
         if(this.blackHole) this.scene.remove(this.blackHole.instance)
 
-        // this = new Galaxy(parameters)
-        return new Galaxy(parameters)
-        // this.setRandomAttributes()
-        // this.setMaterial()
-        // this.setPoints()
+        this.setGeometry()
+        this.setBlackHole()
+        this.setMaterial()
+        this.setPoints()
 
     }
 }
--- src/Experience/Scene/Universe.js
+++ src/Experience/Scene/Universe.js
@@ -8,69 +8,10 @@ export default class Universe {
         this.experience = new Experience()
         this.scene = this.experience.scene
         this.resources = this.experience.resources
-        this.debug = this.experience.debug
-        this.debugFolder = this.debug.ui.addFolder('Galaxy')
-
-        this.parameters = {
-            count: 100000,
-            size: .01,
-            radius: 5,
-            branches: 3,
-            curvature: 1,
-            randomness: .2,
-            concentration: 3,
-            innerColor: 0xff6030,
-            outerColor: 0x1b3984,
-            isBlackHole: false,
-            branchWaves: true,
-            starShape: 4,
-            backdropStarsCount: 10000
-        }
 
         this.resources.on('ready', () => {
             this.backdrop = new Backdrop(10000)
-            this.galaxy = new Galaxy(this.parameters)
-        })
-
-        this.debugFolder.add(this.parameters, 'count', 100, 1000000, 100).onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.add(this.parameters, 'size', .001, .1, .001).onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.add(this.parameters, 'radius', .01, 20, .01).onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.add(this.parameters, 'branches', 2, 20, 1).onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.add(this.parameters, 'curvature', -5, 5, .01).onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.add(this.parameters, 'randomness', 0, 2, .01).onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.add(this.parameters, 'concentration', 1, 10, .1).onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.addColor(this.parameters, 'innerColor').onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.addColor(this.parameters, 'outerColor').onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.add(this.parameters, 'isBlackHole').onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.add(this.parameters, 'branchWaves').onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
-        })
-        this.debugFolder.add(this.parameters, 'starShape', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]).onFinishChange(() => {
-            this.galaxy = this.galaxy.updateGalaxy(this.parameters)
+            this.galaxy = new Galaxy()
         })
     }
-
-    // generateGalaxy() {
-    //     this.galaxy = new Galaxy()
-    // }
 }
