ca077b13451d3f768ff5fd68b5bca2c6c4162824
Josiah Webb <jj.webb520@gmail.com>
2022-11-17 16:14:56 -0700

    deletes unused elements of renderer

--- src/Experience/Renderer.js
+++ src/Experience/Renderer.js
@@ -16,12 +16,6 @@ export default class Renderer {
         this.instance = new THREE.WebGLRenderer({
             canvas: this.canvas
         })
-        this.instance.physicallyCorrectLights = true
-        this.instance.outputEncoding = THREE.sRGBEncoding
-        this.instance.toneMapping = THREE.CineonToneMapping
-        this.instance.shadowMap.enabled = true
-        this.instance.shadowMap.type = THREE.PCFSoftShadowMap
-        // this.instance.setClearColor(0x211d20)
         this.instance.setSize(this.sizes.width, this.sizes.height)
         this.instance.setPixelRatio(this.sizes.pixelRatio)
     }
